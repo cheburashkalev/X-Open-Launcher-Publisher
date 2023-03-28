56cd88fb14b6105ccdc439c2689718784469fd8c
Wellsaik <1play2fun123@gmail.com>
2023-03-28 19:05:50 +0300

    SplitFile and CombineFile

--- X-Open-Launcher-Publisher/Data/GamePacker.cs
+++ X-Open-Launcher-Publisher/Data/GamePacker.cs
@@ -32,5 +32,50 @@ namespace X_Open_Launcher_Publisher.Data
             };
             storage.SetMetadata(uploadedFile.Id, doc);
         }
+
+        public static void SplitFile(string sourceFile, string destinationDirectory, long chunkSize = 2L * 1024L * 1024L * 1024L)
+        {
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.Open))
+            {
+                byte[] buffer = new byte[chunkSize];
+                int bytesRead;
+
+                for (int i = 0; (bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0; i++)
+                {
+                    string destinationFile = Path.Combine(destinationDirectory, $"part_{i}.dat");
+
+                    using (FileStream destinationStream = new FileStream(destinationFile, FileMode.Create))
+                    {
+                        destinationStream.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+        public static void CombineFile(string sourceDirectory, string destinationFile)
+        {
+            using (FileStream destinationStream = new FileStream(destinationFile, FileMode.Create))
+            {
+                int i = 0;
+                foreach (string sourceFile in Directory.GetFiles(sourceDirectory))
+                {
+                    FileInfo fileInfo = new FileInfo(sourceFile);
+                    if (fileInfo.Exists && fileInfo.Extension == ".dat" && fileInfo.Name == $"part_{i}")
+                    {
+                        using (FileStream sourceStream = new FileStream(sourceFile, FileMode.Open))
+                        {
+                            byte[] buffer = new byte[sourceStream.Length];
+                            int bytesRead;
+
+                            while ((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                destinationStream.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
     }
 }
